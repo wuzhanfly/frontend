cb9b978bd6dbfb55ea2d75f226b72a62aebb6ce0
JOKER-CARRY <57023679+JOKER-CARRY@users.noreply.github.com>
2025-11-27 23:56:06 +0800

    Jan
    -- 修复打包配置

--- tools/scripts/build.docker.sh
+++ tools/scripts/build.docker.sh
@@ -1,4 +1,4 @@
 rm -rf ./public/assets/multichain
 rm -rf ./public/assets/envs.js
 
-docker build --progress=plain --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t blockscout-frontend:local ./
+docker build --progress=plain --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0 --always) -t blockscout-frontend:local ./
